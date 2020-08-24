ce3d3126a049f204c940b0161581fb206dc709e0
afifimaduddin <afif.imaduddin2104@gmail.com>
2020-08-24 14:16:01 +0700

    Update document.blade.php

--- resources/views/client/profile/document.blade.php
+++ resources/views/client/profile/document.blade.php
@@ -12,42 +12,22 @@
                     <div class="col-md-12 card-body">
                         @include('client/profile/_topbar')
                         <div class="row p-3">
-                            <div class="mb-3 row">
-                                <label for="staticEmail" class="col-sm-2 col-form-label">Upload KTP</label>
-                                <div class="form-file">
-                                    <input type="file" class="form-file-input" id="customFile">
-                                    <label class="form-file-label" for="customFile">
-                                      <span class="form-file-text">Choose file...</span>
-                                      <span class="form-file-button">Browse</span>
-                                    </label>
-                                  </div>
-                            </div>
-                            <div class="mb-3 row">
-                                <img src="..." class="img-thumbnail" alt="...">
-                            </div>
-                            {{-- <div class="mb-3 row">
+                            <div class="col-sm-4 row">
                                 <label for="staticEmail" class="col-sm-2 col-form-label">Foto KTP</label>
-                                <div class="col-sm-10">
-                                    <p id="addBirthday" class="col-form-label">331419**********</p>
-                                </div>
-                            </div> --}}
-                            <div class="mb-3 row">
-                                <label for="staticEmail" class="col-sm-2 col-form-label">Upload NPWP</label>
-                                <div class="form-file">
-                                    <input type="file" class="form-file-input" id="customFile">
-                                    <label class="form-file-label" for="customFile">
-                                      <span class="form-file-text">Choose file...</span>
-                                      <span class="form-file-button">Browse</span>
-                                    </label>
+                            </div>
+                                  <div class="col-md-4 row">
+                                      <img src="{{asset('image/default_toped-21.jpg')}}" class="img-thumbnail" alt="" srcset="">
                                   </div>
+                            <div class="col-sm-4 row">
+                                <label for="staticEmail" class="col-sm-2 col-form-label">Foto NPWP</label>
                             </div>
-                            <div class="mb-3 row">
-                                <img src="..." class="img-thumbnail" alt="...">
-                            </div>
+                                  <div class="col-md-4 row">
+                                      <img src="{{asset('image/default_toped-21.jpg')}}" class="img-thumbnail" alt="" srcset="">
+                                  </div>
                             <div class="mb-3 row">
                                 <div class="col-md-12 text-right">
                                 <button type="button" class="btn btn-outline-success" data-toggle="modal" data-target="#staticBackdrop">
-                                    ubah dokumen
+                                    upload dokumen
                                 </button>
                                 </div>
                             </div>
@@ -61,7 +41,7 @@
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="staticBackdropLabel">Ubah Dokumen</h5>
+        <h5 class="modal-title text-success" id="staticBackdropLabel">Upload Dokumen</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -69,14 +49,24 @@
       <div class="modal-body">
         <!--isi modal-->
         <div class="row g-3">
-            <div class="col-md-12">
-                <input type="text" class="form-control" placeholder="Nomor KTP" aria-label="Nomor KTP">
+          <label for="staticEmail" class="col-sm-2 col-form-label">Foto KTP</label>
+          <div class="form-file">
+            <input type="file" class="form-file-input" id="customFile">
+            <label class="form-file-label" for="customFile">
+              <span class="form-file-text">Choose file...</span>
+              <span class="form-file-button">Browse</span>
+            </label>
+          </div>
+          <label for="staticEmail" class="col-sm-2 col-form-label">Foto NPWP</label>
+            <div class="form-file">
+              <input type="file" class="form-file-input" id="customFile">
+              <label class="form-file-label" for="customFile">
+                <span class="form-file-text">Choose file...</span>
+                <span class="form-file-button">Browse</span>
+              </label>
             </div>
-            <div class="col-md-12">
-                <input type="text" class="form-control" placeholder="Nomor NPWP" aria-label="Nomor NPWP">
-            </div>
+          </div>
         </div>
-      </div>
       <div class="modal-footer">
         <!-- <button type="button" class="btn btn-danger" data-dismiss="modal">Tutup</button> -->
         <button type="button" class="btn btn-success">Simpan</button>
