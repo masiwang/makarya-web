1a1cdfa9f482df193d64864f21e489092c706763
afifimaduddin <afif.imaduddin2104@gmail.com>
2020-09-03 21:45:05 +0700

    Update checkout.blade.php

--- resources/views/client/market/checkout.blade.php
+++ resources/views/client/market/checkout.blade.php
@@ -21,6 +21,21 @@
                                 Lowokwaru, Kota Malang, 65144</div>
                               <hr style="color: green">
                               <button type="button" class="btn btn-success" data-toggle="modal" data-target="#staticBackdrop">Ubah Alamat</button>
+                              <div style="height:20px"></div>
+                              <div class="card mb-3" style="max-width: 540px;">
+                                <div class="row g-0">
+                                  <div class="col-md-4">
+                                    <img src="{{asset('image/capcay.jpg')}}" class="img-fluid" alt="...">
+                                  </div>
+                                  <div class="col-md-8">
+                                    <div class="card-body">
+                                      <h6 class="card-title">SAYUR CAPCAY PAKET LENGKAP - Sayur Segar</h6>
+                                      <p class="card-text" style="color: orange">Rp,10.000.</p>
+                                      <p class="text-muted">3 Barang</p>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
                             </div>
                             
                             <div class="col-sm-5">
