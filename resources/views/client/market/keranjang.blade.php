237a668bde8c28cebc1c95ce4212c604ed29a760
afifimaduddin <afif.imaduddin2104@gmail.com>
2020-09-26 15:51:07 +0700

    Update keranjang.blade.php

--- resources/views/client/market/keranjang.blade.php
+++ resources/views/client/market/keranjang.blade.php
@@ -93,6 +93,38 @@
                                 </div>
                               </div>
                             </div>
+                            <nav id="navbar-example2" class="navbar navbar-light bg-light px-3">
+                              <a class="navbar-brand" href="#">Navbar</a>
+                              <ul class="nav nav-pills">
+                                <li class="nav-item">
+                                  <a class="nav-link" href="#fat">@fat</a>
+                                </li>
+                                <li class="nav-item">
+                                  <a class="nav-link" href="#mdo">@mdo</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                  <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-expanded="false">Dropdown</a>
+                                  <ul class="dropdown-menu dropdown-menu-right">
+                                    <li><a class="dropdown-item" href="#one">one</a></li>
+                                    <li><a class="dropdown-item" href="#two">two</a></li>
+                                    <li><hr class="dropdown-divider"></li>
+                                    <li><a class="dropdown-item" href="#three">three</a></li>
+                                  </ul>
+                                </li>
+                              </ul>
+                            </nav>
+                            <div data-spy="scroll" data-target="#navbar-example2" data-offset="0">
+                              <h4 id="fat">@fat</h4>
+                              <p>...</p>
+                              <h4 id="mdo">@mdo</h4>
+                              <p>...</p>
+                              <h4 id="one">one</h4>
+                              <p>...</p>
+                              <h4 id="two">two</h4>
+                              <p>...</p>
+                              <h4 id="three">three</h4>
+                              <p>...</p>
+                            </div>
                     </div>
                     <div style="height:50px"></div>
                     <nav aria-label="Page navigation example">
@@ -113,4 +145,5 @@
         </div>
     </div>
     <div style="height:50px"></div>
+    
 @endsection
